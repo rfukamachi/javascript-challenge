8ff2f6d5f1190f61e7106be17739eb18a29eee14
Rika Fukamachi <rika.fukamachi@gmail.com>
2019-08-23 21:49:04 -0700

    commenting in javascript plan and added selectDate fqn

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+//read in the table from data.js... into tableData...
+
+//format it to display all on html...
+
+//function to filter by date:
+//mm/dd/yyyy
+function selectDate(date_input) {
+    return tableData.datetime === date_input;
+}
+
+var new_date_table = tableData.filter(selectDate);
+
+
+
+//debug stmts:
+console.log(tableData[city]);

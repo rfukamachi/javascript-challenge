9d8d139ec490a43cbe6f71f18f29aff22c1c3d04
Rika Fukamachi <rika.fukamachi@gmail.com>
2019-08-25 14:32:18 -0700

    completed UFO level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,21 +1,74 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
-//read in the table from data.js... into tableData...
-
-//format it to display all on html...
-
-//function to filter by date:
-//mm/dd/yyyy
-function selectDate(date_input) {
-    return tableData.datetime === date_input;
-}
-
-var new_date_table = tableData.filter(selectDate);
+// select the button:
+var filterButton = d3.select(".btn");
+// DTM: date time input field:
+var inputDate = d3.select("#datetime");
+// get a reference to the table body:
+var tbody = d3.select("tbody");
 
 
+function displayFullTable() {
+    // use d3 to append 1 cell per report value:
+    tableData.forEach((report) => {
+        // console.log(report);
+        var trow = tbody.append('tr');
+        Object.entries(report).forEach(([key, value]) => {
+            // console.log(key, value);
+            var tcell = trow.append('td');
+            tcell.text(value);
+        });
+    });
+};
 
-//debug stmts:
-console.log(tableData[city]);
+//display full table when first opening the page:
+displayFullTable();
+
+//handler to filter the list accordingly 
+//once the button is clicked:
+function handleFilterClick() {
+    
+    // retrieve the value in the input field:
+    var newDate = inputDate.property('value');
+
+    // if no entry in date field:    
+    if (!newDate) {
+        displayFullTable();
+    } 
+
+    //otw display the filtered table:
+    else {
+        // filter the table data based on newDTM
+        var filteredTable = tableData.filter(siting => siting.datetime === newDate);
+
+        // clear the table before refilling it with filtered data
+        tbody.html("");
+        // populate the table:
+        filteredTable.forEach((report) => {
+            trow = tbody.append('tr');
+            Object.entries(report).forEach(([key, value]) => {
+                tcell = trow.append('td');
+                tcell.text(value);
+            });
+        });
+
+        //debug stmt:
+        console.log(inputDate);
+        console.log('New DTM: ', newDate);
+        console.log(filteredTable);
+    }
+};
+
+filterButton.on('click', handleFilterClick);
+
+
+
+// inputDate.addEventListener("keyup", function(event) {
+//     //if user presses enter, do nothing:
+//     if (event.keyCode === 13) {
+//         event.preventDefault();
+//         handleFilterClick();
+//     }
+// });
+
